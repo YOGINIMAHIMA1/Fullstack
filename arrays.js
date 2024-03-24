7b3e85ea78a2b5e38c39cc1f971f858160a935f5
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2024-03-25 02:30:35 +0530

    Update arrays.js

---
+++ arrays.js
@@ -0,0 +1,26 @@
+let students =["Arshita","Divya","Riya","kiara"]
+console.log(students)
+console.log(students[0])
+console.log(students.length)
+students[2]="kiara advani"
+console.log(students)
+students=["mahima","Rihanna","Diandra"]
+console.log(students)
+console.log(students.push("nancy"))
+console.log(students)
+console.log(students.reverse())
+const myArr=[1,"Maya",true,{name:'kiara', age:30}]
+console.log(myArr)
+console.log
+(myArr.indexOf("Maya"));
+myArr.pop();
+console.log(myArr)
+
+
+const  Arr1=["Maya","Zorro",true,1,"kuldeep"]
+
+
+Arr1.forEach((Arr)=>
+{  
+  console.log(Arr)
+});
