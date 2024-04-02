47f7daf9da09337b6f5f869a034bf89ddab5756d
WOMENWHOCODE <mahimabhardwajmahimahardwaj@gmail.com>
2024-04-03 01:00:54 +0530

    updated promises.js

---
+++ Promise.js
@@ -0,0 +1,16 @@
+const posts = [
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post Two', body: 'This is post two' },
+  ];
+  
+  function getPosts() {
+     setTimeout(()=> {
+        let output='';
+        posts.forEach((post)=>
+        {
+          output+=`<li>${post.title}</li>`
+        })
+        document.body.innerHTML=output;
+  },1000);
+  }
+  getPosts();
