5879d0afdafc02dbcf4e72d5ef2e66de9350620d
WOMENWHOCODE <mahimabhardwajmahimahardwaj@gmail.com>
2024-04-06 12:24:53 +0530

    updated promises.js

--- Promise.js
+++ Promise.js
@@ -1,48 +1,29 @@
-const posts = [
-    { title: 'Post One', body: 'This is post one' },
-    { title: 'Post Two', body: 'This is post two' },
-  ];
-  
-  function getPosts() {
-     setTimeout(()=> {
-        let output='';
-        posts.forEach((post)=>
-        {
-          output+=`<li>${post.title}</li>`
-        })
-        document.body.innerHTML=output;
-  },1000);
-  }
-  getPosts();
-
-  const student =[
-    {id:1 , name:'hkirat'},
-    {id:2,name:'komal'},
-    {id:3,name:'rita'},
-    ];
-
-    function data(){
-        
-        setTimeout(()=>
-        {
-           let out='' 
-            student.forEach((data1)=>
-            {
-               out+= `<li>${data1.id}</li>`
-            })
-            document.body.innerHTML=out;
-        },2000)
-        
-    }
-    data();
-
-        
-   function createPost(post1)
-  {
-      setTimeout(()=>
-      {
-         posts.push(post1) 
-      },1000)
-  }
-  createPost({title:'Post three', body:'This is post three'})
-  
+const posts=[
+   { title:'post one',body:'This is body one'},
+   {title:'Post two', body:'This is body two'},
+   ]
+   function getPosts(post)
+   {
+     setTimeout(()=>
+     {
+       let output='';
+       posts.forEach((post)=>
+       {
+         output+=`<li> ${post.title}</li>`
+       });
+       document.body.innerHTML=output;
+       
+     },1000)
+   }
+   
+   
+   function createPost(post,callback)
+   {
+     setTimeout(()=>
+     {
+       posts.push(post)
+       callback();
+     },2000)
+   }
+   createPost({title:'Post Three',body:'This is post three', body:'this is post Three'},getPosts)
+   
