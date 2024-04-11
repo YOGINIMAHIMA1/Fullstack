631b1bfc7adcea17124a1118f626fd97457091d5
WOMENWHOCODE <mahimabhardwajmahimahardwaj@gmail.com>
2024-04-11 19:22:49 +0530

    update promise1.js

--- promise1.js
+++ promise1.js
@@ -27,12 +27,64 @@ getData();
  console.log(result)
  */
 
- fetch("https://jsonplaceholder.typicode.com/posts").then((data)=>
- {
-   console.log(data)
- })
- .catch(error=>
+
+//callback hell se bachne ke liye promise aya, promise ke teen state hote hai -pending, fullfilled , rejected
+// promise asynchronous task hai aur javascript synchrounous hai matlab javascript single threaded hai
+// asynchronous me task so simple hota hai voh pehele execute ho jata hai
+
+let myPromise= new Promise((resolve,reject)=>
 {
-   console.log(error)
+   const error=false;
+   if(!error)
+   {
+      resolve("yes!resolved the promise")
+   }
+   else
+   {
+      reject("not ressolved the promise")
+   }
+})
+console.log(myPromise)
+
+// .then ka ujse hota hai value ko return karne ke liye
+myPromise.then((value)=>
+{
+   return value + 1;
+}).then((newValue)=>
+{
+   console.log(newValue)
+})
+fetch("https://jsonplaceholder.typicode.com/posts").then((data)=>
+{
+  console.log(data)
+})
+.catch(error=>
+{
+  console.log(error)
 })
 console.log(result)
+
+// to catch the error
+ myPromise= new Promise((res,rej)=>
+{
+  let error=true;
+  if(!error)
+  {
+    res('yes there is error')
+  }
+  else
+  {
+    rej('no error')
+  }
+})
+console.log(myPromise)
+myPromise.then((value)=>
+{
+  return value;
+}).then((newValue)=>
+{
+  console.log(newValue)
+}).catch(err=>
+{
+  console.error(err)
+})
