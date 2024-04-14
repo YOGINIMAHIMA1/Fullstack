5127d0fe7b1aad8b7d5094d03bd1809103d4a03d
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2024-04-14 14:51:11 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,27 @@
+let str="yashika"
+
+console.log(str.length)
+console.log(str.indexOf('s'))
+
+console.log(str.charAt(1))
+
+
+x= str.slice(2,5)
+console.log(x)
+console.log(str)
+x= str.substring(3,6)
+console.log(x)
+
+x= str.replace('him','she')
+console.log(x)
+
+x= str.valueOf();
+console.log(x)
+
+x= str.split('')
+console.log(x)
+
+str= 'womenwhocode'
+x=str.charAt(0).toUpperCase()
+x= x+ str.substring(1,12)
+console.log(x)
